6c113849da4e09c3a0fd9a09cda6b505996cdaa3
BY <pby1360@gmail.com>
2023-02-03 01:14:15 +0900

    #2 add bootstrap

--- frontend/src/components/signUp.tsx
+++ frontend/src/components/signUp.tsx
@@ -1,10 +1,33 @@
 
 import React from 'react';
+import { Form, Button } from 'react-bootstrap';
 
+import './component.scss';
 const signUp = () => {
   return (
-    <div>
-      회원가입
+    <div className='join'>
+      <section className='info'></section>
+      <section className='form'>
+        <Form className='d-grid p-5'>
+          <Form.Group className="mb-3">
+            <Form.Label>Id</Form.Label>
+            <Form.Control type="email" />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Password</Form.Label>
+            <Form.Control required type="password" />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control required type="password" />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Name</Form.Label>
+            <Form.Control required as="input" />
+          </Form.Group>
+          <Button size='lg' type='submit'>Sign-up</Button>
+        </Form>
+      </section>
     </div>
   );
 };
--- frontend/src/layout/topBar.tsx
+++ frontend/src/layout/topBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './layout.scss';
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 type TopBarProps = {
   isLoggedIn: boolean;
@@ -13,8 +14,11 @@ const topBar: React.FC<TopBarProps> = ({isLoggedIn}) => {
       <a href='/'><span className='logo'></span></a>
       <div className='control-right'>
         {isLoggedIn ? <button className='info'>Info</button> : null}
-        <button className='login'><Link to={'/sign-in'}>Sign-in</Link></button>
-        <button className='logout'><Link to={'/sign-up'}>Sign-up</Link></button>
+        {/* <Button variant='primary' className='login'><Link to={'/sign-in'}>Sign-in</Link></Button> */}
+        {/* <Button variant='outline-primary' className='logout'><Link to={'/sign-up'}>Sign-up</Link></Button> */}
+        <Button variant='primary' className='login' href='/sign-in'>Sign-in</Button>
+        <Button variant='outline-primary' className='logout'><Link to={'/sign-up'}>Sign-up</Link></Button>
+        
       </div>
     </div>
   );
